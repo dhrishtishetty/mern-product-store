d408040c3605a4ef47fc7d507be3e0c3bf7e11ad
dhrishtishetty <dhrishtishetty10@gmail.com>
2026-06-12 13:07:10 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -13,7 +13,7 @@ const HomePage = () => {
   console.log("products", products);
 
   return (
-    <Container maxW='container.xl' py={6}>
+    <Container maxW='container.xl' py={10}>
       <VStack spacing={8}>
         <Text
           fontSize={"28"}
